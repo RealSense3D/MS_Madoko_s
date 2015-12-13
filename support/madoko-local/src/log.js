db6ccb17dbfb3788378a8b7d8c7feb806d0a26b9
daan <daan@localhost>
2015-12-13 14:05:25 -0800

    Support CSS page-break-before and page-break-after in PDF

---
+++ support/madoko-local/src/log.js
@@ -0,0 +1,98 @@
+/*---------------------------------------------------------------------------
+  Copyright 2015 Microsoft Corporation.
+ 
+  This is free software; you can redistribute it and/or modify it under the
+  terms of the Apache License, Version 2.0. A copy of the License can be
+  found in the file "license.txt" at the root of this distribution.
+---------------------------------------------------------------------------*/
+
+if (typeof define !== 'function') { var define = require('amdefine')(module) }
+define([],function() {
+
+var Util = require("./util.js");
+
+// -------------------------------------------------------------
+// Logging
+// -------------------------------------------------------------
+
+var Log = (function(){
+
+  function Log(verbose,dir,flushIval,base) {
+    var self = this;
+    self.verbose = verbose;
+    self.dir  = dir;
+    self.base = base || "log-";    
+    self.start(flushIval || 60000);
+  }
+
+  Log.prototype.message = function( msg, level, logfileLevel ) {
+    var self = this;
+    if (level==null) level = 0;
+    if (logfileLevel==null) logfileLevel = 0;
+    if (logfileLevel <= self.verbose) {
+      self.entry( { type: "message", level: level, message: msg, /*date: new Date().toISOString()*/ });
+    }
+    if (level <= self.verbose) {
+      var pre = (typeof level !== "number" || level <= 0 ? "" : Array(level+1).join("-") + " ");
+      console.log( pre + msg );
+    }
+  }
+
+  Log.prototype.info = function(msg,ll) {
+    var self = this;
+    self.message(msg,1,ll);
+  };
+
+  Log.prototype.trace = function(msg,ll) {
+    var self = this;
+    self.message(msg,2,ll);
+  };
+
+  Log.prototype.start = function(flushIval) {
+    var self = this;
+    if (self.ival) {
+      clearInterval(self.ival);
+      flush();
+    }
+    
+    self.log = [];
+    self.ival = setInterval( function() {
+      self.flush();      
+    }, flushIval );
+  }
+
+  Log.prototype.flush = function() {
+    var self=this;
+    if (!self.log || self.log.length <= 0) return;
+
+    var content = self.log.join("\n") + "\n";
+    var date = new Date().toISOString().replace(/T.*/,"");
+    var logFile = Util.combine( self.dir, self.base + date + ".txt");
+    return Util.appendFile(logFile, content, {encoding:"utf8",ensuredir: true} ).then( function() {
+      return;
+    }, function(err) {
+      console.log("unable to write log data to " + logFile + ": " + err);
+    });
+    self.log = []; // clear log
+  }
+
+  Log.prototype.entry = function( obj, showConsole ) {
+    var self = this;
+    if (!obj) return;
+    var data = JSON.stringify(obj);
+    if (showConsole) console.log( data + "\n" );
+    if (obj.type != "none" && self.log[self.log.length-1] !== obj) {
+      self.log.push( data );
+    }
+  }
+
+  return Log;
+})();
+
+
+// module interface
+return {
+  Log: Log,
+};
+
+});
